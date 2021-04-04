326c0f56a9c4f832353688422d2d04cf51b1458f
luxumbra <me@luxumbra.dev>
2021-04-04 13:54:54 +0100

    CHanged the homepage meta so it is included at build time

--- src/Components/PageMeta.js
+++ src/Components/PageMeta.js
@@ -2,71 +2,59 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Helmet from 'react-helmet'
 
+export const homeMetaData = {
+    name: "$100 BEES - Hexel Collection",
+    description: "There's over 20,000 bee species in the world, get to know some of them! The crypto bee collective engages in bee conservation cryptophilanthropy. At least 50% of the proceeds will be donated to bee conservation. The remainder will go to support the artist and the bee collective. Bzzzz",
+    banner: "https://lh3.googleusercontent.com/Eqd6B5-74Ti4YHjKmDVuhJa9XMcRYijTr0GKgsLVGNQ1GEk0RHRSGfisa8cC8bMmay5HLR58qrGtFPgyTemjhXh3Jnipk4ooSHJT_hk=s2500"
+}
 export function HomeMeta() {
-    const [collectionMeta, setCollectionMeta] = useState()
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        axios.get(`https://api.opensea.io/api/v1/collections?asset_owner=0x9f9662d40f4dc862f0a1d43bf6b1e4ac200ae1af&slug=100-bees-hexel-collection`).then(response => {
-            console.log(response.data)
-            // debugger
-            setCollectionMeta(response.data)
-        }).catch(error => {
-            console.log(error)
-        }).then(() => setLoading(false))
-    }, [])
     return (
-        <>
-            {!loading && (
-                <Helmet>
+        <Helmet>
 
-                    {/* <!-- HTML Meta Tags --> */}
-                    <title>{collectionMeta[0].name} - Hundred Dollar Bees</title>
-                    <meta
-                        name="description"
-                        content={collectionMeta[0].description}
-                    />
+            {/* <!-- HTML Meta Tags --> */}
+            <title>{homeMetaData.name} - Hundred Dollar Bees</title>
+            <meta
+                name="description"
+                content={homeMetaData.description}
+            />
 
-                    {/* <!-- Google / Search Engine Tags --> */}
-                    <meta property="name" content={collectionMeta[0].name} />
-                    <meta
-                        property="description"
-                        content={collectionMeta[0].description}
-                    />
-                    <meta
-                        property="image"
-                        content={collectionMeta[0].banner_image_url}
-                    />
+            {/* <!-- Google / Search Engine Tags --> */}
+            <meta property="name" content={homeMetaData.name} />
+            <meta
+                property="description"
+                content={homeMetaData.description}
+            />
+            <meta
+                property="image"
+                content={homeMetaData.banner}
+            />
 
-                    {/* <!-- Facebook Meta Tags --> */}
-                    <meta property="og:url" content="https://hundreddollarbees.io/" />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:title" content={collectionMeta[0].name} />
-                    <meta
-                        property="og:description"
-                        content={collectionMeta[0].description}
-                    />
-                    <meta
-                        property="og:image"
-                        content={collectionMeta[0].banner_image_url}
-                    />
+            {/* <!-- Facebook Meta Tags --> */}
+            <meta property="og:url" content="https://hundreddollarbees.io/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={homeMetaData.name} />
+            <meta
+                property="og:description"
+                content={homeMetaData.description}
+            />
+            <meta
+                property="og:image"
+                content={homeMetaData.banner}
+            />
 
-                    {/* <!-- Twitter Meta Tags --> */}
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:title" content={collectionMeta[0].name} />
-                    <meta
-                        name="twitter:description"
-                        content={collectionMeta[0].description}
-                    />
-                    <meta
-                        name="twitter:image"
-                        content={collectionMeta[0].banner_image_url}
-                    />
+            {/* <!-- Twitter Meta Tags --> */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={homeMetaData.name} />
+            <meta
+                name="twitter:description"
+                content={homeMetaData.description}
+            />
+            <meta
+                name="twitter:image"
+                content={homeMetaData.banner}
+            />
 
-                </Helmet>
-            )
-            }
-        </>
+        </Helmet>
     )
 }
 
